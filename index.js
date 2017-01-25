1594a75c386a48d0bf1cd24d36649f314d0d192a
Liliana Alonzo <lillyneveu@gmail.com>
2017-01-25 23:35:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var index = [];
+
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  var array = [2, 4, 6, 8];
+  array.forEach(callback);
+    return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
